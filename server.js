d8ff66c43063182207879bce9bbdcad34c795fba
Tommy W <wikstromtommy3@gmail.com>
2023-01-30 13:54:21 +0100

    4

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+//test
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
